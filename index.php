9eb1a0e03cb2e3aa254bdb953e4beadd19720092
Dwisans <wiwik1161@gmail.com>
2024-07-09 12:40:44 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa nobis error porro, corporis suscipit sapiente explicabo officia vel consectetur illum non nihil ab perspiciatis eum aliquam voluptas alias sint pariatur?</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
